b5fb9170245453ab6549a95f797d30cfac83250c
Ilvar Santiago Camelo Amaya <ilvarcamelo206@gmail.com>
2023-10-10 16:53:36 -0500

    Estamos perdidas, perdidas

--- parcial.cpp
+++ parcial.cpp
@@ -6,16 +6,23 @@ using namespace std;
 
 string read(string message);
 int menu();
+void subjectsValidation(int numberOfSubjects);
+void gradesBySubject(int numberOfSubjects);
+double gradesValidation(double grade);
+void percentageValidation();
 
 int main(){
 int op;
+int numberOfSubjects;
 
     do{
         op = menu();
         switch (op)
         {
         case 1:
-            cout << "Calcula nota";
+            cout << "\n Calcula nota \n";
+             numberOfSubjects = stoi(read("Digite numero de materias a evaluar: "));
+             subjectsValidation(numberOfSubjects);
             break;
         case 0: 
             cout << "Salida";
@@ -52,5 +59,57 @@ int menu(){
     option = stoi(read("Digite opcion: "));
     return option;
 }
+
+void subjectsValidation(int numberOfSubjects){
+
+    if(numberOfSubjects > 0 && numberOfSubjects < 6){
+        gradesBySubject(numberOfSubjects);
+        
+    }else{
+        do{
+        cout << "El numero de materias debe estar entre 1 y 5 \n";
+        numberOfSubjects = stoi(read("Digite numero de materias a evaluar: "));
+        
+    }while(numberOfSubjects < 0 & numberOfSubjects >5);
+    }
+
+    gradesBySubject(numberOfSubjects);
+
+
+}
+
+void gradesBySubject(int numberOfSubjects){
+    double grade, gradeAccumulator;
+    int i = 0;
     
+    for(int i = 1; i < numberOfSubjects; i++){
+        grade = stod(read("Digite nota de la materia: "));
+        gradeAccumulator += gradesValidation(grade);
+
+
+    }    
+
+    
+
+    }
+
+    double gradesValidation(double grade){
+        if(grade > 0.0 && grade < 5.1){
+
+            return grade;
+
+
+        }else{
+            do{cout << "El numero de materias debe estar entre 1 y 5 \n";
+        grade = stoi(read("Digite numero de materias a evaluar: "));}
+        while(grade < 0.0 && grade > 5.0);}
+        return grade; 
+
+    }
+
+    void percentageValidation(){
+        int percentageSubject1, percentageSubject2
+        percentageSubject1 = stoi(read("Digite porcentaje de la materia 1: "))
+
+    }
 
